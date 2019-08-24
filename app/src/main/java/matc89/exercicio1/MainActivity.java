4db0a83b101d6bb4b86178e25c55cab2a790d909
Matheus <matheus_lima789@hotmail.com>
2019-08-24 20:16:47 -0300

    Solucao 1

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,12 +2,30 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView labelMensagem;
+    private Button btnCumprimentar;
+    private EditText editNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCumprimentar = (Button) findViewById(R.id.btnCumprimentar);
+        labelMensagem = (TextView) findViewById(R.id.labelMensagem);
+        editNome = (EditText) findViewById(R.id.editNome);
     }
+
+        public void clicou(View v) {
+            String textoDigitado = editNome.getText().toString();
+            labelMensagem.setText("Alô, "+textoDigitado+"!");
+        }
 }
+
